803682b1d4bd4ca4a459dbe45b0b2973600ed685
Shayan Golafshani <sgolafshani@cnm.edu>
2020-10-26 07:56:41 -0600

    Added markdown files, still thinking about how to properly implement queries....

--- app/src/main/java/edu/cnm/deepdive/keepintouch/model/entity/AutoReply.java
+++ app/src/main/java/edu/cnm/deepdive/keepintouch/model/entity/AutoReply.java
@@ -1,8 +1,30 @@
 package edu.cnm.deepdive.keepintouch.model.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
 
-@Entity()
+@Entity(
+    indices = @Index(value = "message", unique = true),
+    foreignKeys = {
+        //same reasoning I used in the User entity applies here.
+        @ForeignKey(
+            entity = UserType.class,
+            childColumns = "user_type_id",
+            parentColumns = "user_type_id"
+        )
+    }
+)
 public class AutoReply {
+
+  @ColumnInfo(name = "message")
+  private String message;
+
+  @NonNull
+  @ColumnInfo(name = "user_type_id")
+  private long userTypeId;
+
 
 }
