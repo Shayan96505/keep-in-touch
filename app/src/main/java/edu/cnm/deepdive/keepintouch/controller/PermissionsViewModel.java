ee36e040262bb7757a17af29f3419e6690648c15
Shayan Golafshani <sgolafshani@cnm.edu>
2020-12-02 06:40:09 -0700

    Tried to add in the functionality for the dialog box

---
+++ app/src/main/java/edu/cnm/deepdive/keepintouch/controller/PermissionsViewModel.java
@@ -0,0 +1,32 @@
+package edu.cnm.deepdive.keepintouch.controller;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import java.util.HashSet;
+import java.util.Set;
+import androidx.lifecycle.ViewModel;
+
+public class PermissionsViewModel extends ViewModel {
+  private final MutableLiveData<Set<String>> permissions = new MutableLiveData<>(new HashSet<>());
+
+  public LiveData<Set<String>> getPermissions() {
+    return permissions;
+  }
+
+  public void grantPermission(String permission) {
+    Set<String> permissions = this.permissions.getValue();
+    //noinspection ConstantConditions
+    if (permissions.add(permission)) {
+      this.permissions.setValue(permissions);
+    }
+  }
+
+  public void revokePermission(String permission) {
+    Set<String> permissions = this.permissions.getValue();
+    //noinspection ConstantConditions
+    if (permissions.remove(permission)) {
+      this.permissions.setValue(permissions);
+    }
+  }
+}
